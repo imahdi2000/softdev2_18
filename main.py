6f0ff857710a470cf1b260c6aba4380c920652cc
imahdi2000 <imahdi@stuy.edu>
2018-04-30 14:51:33 -0400

    i cry

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
 
-def 
+import string
+def filemanager():
+    o = open("napolean.txt", "r")
+    text = o.read()
+    return text
+
+
+def freqency(word):
+    text = filemanager()
+    new_text = string.split(text)
+    z = reduce(lambda x, y: x + y, [1 for something in new_text if something == word])
+    print z
+
+
+freqency("apple")
